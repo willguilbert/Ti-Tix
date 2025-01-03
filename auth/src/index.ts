b370bb534bba5a64a94ea786a19f1612dce10fcf
wguilbert <williamguilbert@hotmail.com>
2025-01-03 14:13:54 -0500

    feat(): skaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,8 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+
+
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log("Listening on port 3000 !!!!!!");
 });
