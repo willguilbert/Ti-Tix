57b756d889e12464ecae9d0c0e969e79c5351d7b
wguilbert <williamguilbert@hotmail.com>
2025-01-04 17:10:56 -0500

    feat(): user model finished

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,22 @@
 import mongoose from "mongoose";
 
+// Interface that describes the properties of a new user
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+// Interface that describes the properties that a User model has
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+// Interface that describes the properties that a User Document has
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -11,6 +28,11 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
 
 export { User };
