9dc67078fde2325349dab75a25f203856cba1cdf
wguilbert <williamguilbert@hotmail.com>
2025-01-04 17:58:14 -0500

    feat(): password hashing in usermodel

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,20 +1,21 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // Interface that describes the properties of a new user
 interface UserAttrs {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 // Interface that describes the properties that a User model has
 interface UserModel extends mongoose.Model<UserDoc> {
-    build(attrs: UserAttrs): UserDoc;
+  build(attrs: UserAttrs): UserDoc;
 }
 
 // Interface that describes the properties that a User Document has
 interface UserDoc extends mongoose.Document {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -26,6 +27,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
