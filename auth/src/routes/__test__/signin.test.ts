6128990d509d3edf3336205857734891da2a10cf
wguilbert <williamguilbert@hotmail.com>
2025-01-05 13:10:33 -0500

    feat(): signin tests

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,35 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("Fails when a email that doesnt exist is supplied", async () => {
+  await request(app)
+    .post("/api/users/signin")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(400);
+});
+
+it("Fails when an incorrect password is supplied", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signin")
+    .send({ email: "test@test.com", password: "bad-password" })
+    .expect(400);
+});
+
+it("Responds with a cookie when given valid credentials", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+
+  const res = await request(app)
+    .post("/api/users/signin")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(200);
+
+  expect(res.get("Set-Cookie")).toBeDefined();
+});
