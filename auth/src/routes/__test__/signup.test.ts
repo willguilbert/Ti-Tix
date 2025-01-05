79b08b88f35fbed203f52c4cb7863169bb1d5d91
wguilbert <williamguilbert@hotmail.com>
2025-01-05 12:44:24 -0500

    feat(): some more signup tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -7,3 +7,21 @@ it("Returns a 201 on successful signup", async () => {
     .send({ email: "test@test.com", password: "password" })
     .expect(201);
 });
+
+it("Returns a 400 on bad email input", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "testtest.com", password: "password" })
+    .expect(400);
+});
+
+it("Returns a 400 on invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "testtest.com", password: "1" })
+    .expect(400);
+});
+
+it("Returns a 400 on no email & no password input", async () => {
+  return request(app).post("/api/users/signup").send({}).expect(400);
+});
