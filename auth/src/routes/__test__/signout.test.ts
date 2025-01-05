e6641fd398189c2455d7a979cc9105787fd52fdb
wguilbert <williamguilbert@hotmail.com>
2025-01-05 13:16:22 -0500

    feat(): signout test

---
+++ auth/src/routes/__test__/signout.test.ts
@@ -0,0 +1,28 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("Clears the cookie after signing out", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signin")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(200);
+
+  const res = await request(app)
+    .post("/api/users/signout")
+    .send({})
+    .expect(200);
+
+  const cookie = res.get("Set-Cookie");
+  if (!cookie) {
+    throw new Error("Expected cookie but got undefined.");
+  }
+
+  expect(cookie[0]).toEqual(
+    "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
+  );
+});
