76933bd0f1abe1cf307be6673b12d4a5243f4e60
wguilbert <williamguilbert@hotmail.com>
2025-01-05 23:50:39 -0500

    feat(): next config

--- client/pages/b.js
+++
@@ -1,5 +0,0 @@
-const Fok = () => {
-  return <h1>Damn!</h1>;
-};
-
-export default { Fok };
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
 const Home = () => {
-  return <h1>Landing Page</h1>;
+  return <h1>Home Page</h1>;
 };
 export default Home;
