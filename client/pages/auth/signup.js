84344d5c7a5e4caa64eacb8b1c3f9ea5fbf2782a
wguilbert <williamguilbert@hotmail.com>
2025-01-22 15:53:24 -0500

    chore(): useRequest custom hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,21 +1,20 @@
 import { useState } from "react";
-import axios from "axios";
+import useRequest from "../../hooks/useRequest";
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: "https://ticketing.dev/api/users/signup",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+  });
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    try {
-      const res = await axios.post("https://ticketing.dev/api/users/signup", {
-        email,
-        password,
-      });
-      console.log(res.data);
-    } catch (e) {
-      setErrors(e.response.data.errors);
-    }
+    doRequest();
   };
 
   return (
@@ -38,16 +37,7 @@ export default () => {
           className="form-control"
         ></input>
       </div>
-      {errors.length !== 0 && (
-        <div className="alert alert-danger">
-          <h4>Oops....</h4>
-          <ul className="my-0">
-            {errors.map((e) => (
-              <li key={e.message}>{e.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
