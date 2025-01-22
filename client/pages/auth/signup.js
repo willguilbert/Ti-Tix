9dd9e2cbd4a42cefdebeb18910bad27206b9086a
wguilbert <williamguilbert@hotmail.com>
2025-01-22 01:02:28 -0500

    feat(): login form

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+        ></input>
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        ></input>
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
